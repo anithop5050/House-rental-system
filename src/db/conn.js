a06b7fd8e501e1b556594ccd90222241b3950ee5
anithop5050 <anithshibu@gmail.com>
2026-01-18 15:11:58 +0530

    Update DB config and API URLs for deployment

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/", {
+mongoose.connect(process.env.DB_URL || "mongodb://localhost:27017/", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-    console.log(`database is connected`);
+  console.log(`database is connected`);
 }).catch((e) => {
-    console.log(e);
+  console.log(e);
 })
